a42a4028c2d81147c0c6e6a3adcce7cb738fe085
jaisarun53 <jaisarun53@gmail.com>
2024-04-21 11:27:44 +0545

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./App.css";
-import ConditionalRender1 from "./ConditionalRender1";
-import ConditinalRender2 from "./ConditinalRender2";
-import ConditionalRender3 from "./ConditionalRender3";
+import ShowName from "./ShowName";
 
 const App = () => {
   return (
     <div>
-      {/* <ConditionalRender1 /> */}
-      <ConditionalRender3 />
-      {/* <ConditinalRender2 /> */}
+      <ShowName studentName="Payal" />
+      <ShowName studentName="Arun" />
     </div>
   );
 };
---
+++ src/ShowName.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ShowName = (props) => {
+  return (
+    <div>
+      <p>Hey {props.studentName}, have a good day</p>
+    </div>
+  );
+};
+
+export default ShowName;
